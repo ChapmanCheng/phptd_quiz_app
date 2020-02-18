2021fc0cf908ea97bb333a3fc8a815fe4cf5a80e
chapmanCC <chapboy1989@gmail.com>
2020-02-19 04:51:37 +0800

    complete

--- inc/class/Answers.php
+++ inc/class/Answers.php
@@ -5,11 +5,12 @@ class Answers
     private $answered;
     private $length;
     private $rand;
+    private $result;
 
     public function __construct($questions)
     {
         if (empty($_SESSION['answered']))
-            $_SESSION['answered'];
+            $_SESSION['answered'] = array();
 
         $this->questions = $questions;
         $this->answered = $_SESSION['answered'];
@@ -31,11 +32,32 @@ class Answers
     }
     public function getUnansweredRandomNum()
     {
-        $allKeys = array_keys($this->questions);
-        $answeredKeys = array_keys($this->answered);
-        $diff = array_diff($allKeys, $answeredKeys);
-        $diff = array_values($diff);
-        $rand = rand(0, count($diff) - 1);
-        $this->rand =  $diff[$rand];
+        if (count($this->answered) != 10) {
+
+            $allKeys = array_keys($this->questions);
+            $answeredKeys = array_keys($this->answered);
+            $diff = array_diff($allKeys, $answeredKeys);
+            $diff = array_values($diff);
+            $rand = rand(0, count($diff) - 1);
+            $this->rand =  $diff[$rand];
+        } else {
+            $rand = rand(0, count($this->questions) - 1);
+            $this->rand = $rand;
+        }
+    }
+    public function checkLastQuestion()
+    {
+        $id = $_POST['id'];
+        $answer = $_POST['answer'];
+
+        return $this->questions[$id]['correctAnswer'] == $answer ? 1 : 0;
+    }
+    public function checkAllQuestions()
+    {
+        $correctAnswers = array_map(function ($qt) {
+            return $qt['correctAnswer'];
+        }, $this->questions);
+        $result = array_intersect($correctAnswers, $this->answered);
+        $this->result = count($result);
     }
 }
--- index.php
+++ index.php
@@ -14,20 +14,33 @@ include_once './inc/questions.php';
 if (isset($_POST['id'])) {
     $post = new PostData();
     $post->pushToSession();
-}
+};
+
+// var_dump($_SESSION['answered']);
 
 $answers = new Answers($questions);
 $answers->sortAnsweredByKey();
 $answers->getUnansweredRandomNum();
 
 $question = $questions[$answers->rand];
+
 include_once './inc/html_blocks/header.php';
 ?>
 
 <div id="quiz-box">
+    <?php
+    if (isset($_GET['score'])) {
+        $answers->checkAllQuestions();
+        echo "<h1>You Got $answers->result Questions Correct</h1>";
+    } else if (isset($_POST['id'])) {
+        $result = $answers->checkLastQuestion();
+        echo $result ? "<h4>You got it right! Hurray</h4>" : "<h4>Boo! You are wrong!</h4>";
+    }
+
+    ?>
     <p class="breadcrumbs">Question <?php echo $answers->length . ' of ' . count($questions); ?></p>
-    <p class="quiz">What is <?php echo $question['leftAdder']; ?> + <?php echo $question['rightAdder']; ?>?</p>
-    <form action="index.php" method="post">
+    <p class="quiz">What is <?php echo $question['leftAdder'] . ' + ' .  $question['rightAdder']; ?>?</p>
+    <form action="<?php $_SERVER["PHP_SELF"]; ?>" method="post">
         <input type="hidden" name="id" value="<?php echo $answers->rand; ?>" />
         <?php
         $nums = array_slice($question, 2);
@@ -36,6 +49,12 @@ include_once './inc/html_blocks/header.php';
             echo "<input type='submit' class='btn' name='answer' value='$num' />";
         ?>
     </form>
+    <?php
+    if ($answers->length == count($questions)) { ?>
+        <a href="<?php $_SERVER["PHP_SELF"]; ?>?score" class="btn">Show score</a>
+        <a href="<?php $_SERVER["PHP_SELF"]; ?>" class="btn">Continue</a>
+    <?php }; ?>
+
 </div>
 <?php
 include_once './inc/html_blocks/footer.php';
