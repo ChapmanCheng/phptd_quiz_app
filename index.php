9c1f304cfb7e42577a354bed2eb586469934d0a7
chapmanCC <chapboy1989@gmail.com>
2020-02-19 01:35:49 +0800

    refactor index.php

--- index.php
+++ index.php
@@ -2,31 +2,44 @@
 session_start();
 
 include_once './inc/quiz.php';
-include_once './inc/questionLottery.php';
+include_once './inc/class/QuestionsHandler.php';
+include_once './inc/class/PostData.php';
+include_once './inc/class/Question.php';
 include_once './inc/questions.php';
 
 // Session
-if (empty($_SESSION)) {
-    // init if session is empty 
-    $_SESSION['lottery'] = new QuestionLottery($questions);
-}
+/**
+ * ! Deprecated
+ * if (empty($_SESSION['answered'])) {
+ *  new QuestionsHandler($questions);
+ * }
+ */
 
-$lottery = new QuestionLottery($questions);
-$q = $lottery->getQuestion();
+
+
+// session_destroy();
+// if (isset($_POST['id'])) {
+//     $postData = new PostData();
+//     $postData->saveToSession();
+// }
+
+
+$question = new Question($questions);
 
 include_once './inc/html_blocks/header.php';
-?>
-<div id="quiz-box">
-    <p class="breadcrumbs"><?php echo 'Question ' . $lottery->getAnswered() . ' of ' . $lottery->getTotal(); ?></p>
-    <p class="quiz"><?php echo "What is $q[leftAdder] + $q[rightAdder]?"; ?></p>
-    <form action="<?php echo "index.php?count=$count"; ?>" method="post">
-        <input type="hidden" name="id" value="0" />
-        <?php
-        $nums = array_slice($q, 2);
-        shuffle($nums);
-        foreach ($nums as $num)
-            echo "<input type='submit' class='btn' name='answer' value='$num' />"
-        ?>
-    </form>
-</div>
-<?php include_once './inc/html_blocks/footer.php'; ?>
+
+echo '
+    <div id="quiz-box">
+        <p>question index is ' . $question->index . ' -- delete this -- </p>
+        <p class="breadcrumbs">Question ' . 1 . ' of ' . $question->total . '</p>
+        <p class="quiz">What is ' . $question->q['leftAdder'] . ' + ' . $question->q['rightAdder'] . '?</p>
+        <form action="index.php" method="post">
+            <input type="hidden" name="id" value="' . $question->index . '" />';
+
+$nums = array_slice($question->q, 2);
+shuffle($nums);
+foreach ($nums as $num)
+    echo "<input type='submit' class='btn' name='answer' value='$num' />";
+
+echo '</form></div>';
+include_once './inc/html_blocks/footer.php';
